e886afde8f85f532f1dc335906966360dfc2e838
Georgiana-Maria Petricele <115110913+georgianapetricele@users.noreply.github.com>
2024-01-29 18:54:16 +0200

    Delete Semester 3 /Functional and Logical Programming directory

--- Semester 3 /Functional and Logical Programming/Prolog/addend.pl
+++
@@ -1,8 +0,0 @@
-%add = e, if l empty
-%    = l1 + add(e,l2..ln)
-
-
-add(E,[],[E]).
-add(E,[H|T],[H|Rez]):-
-    add(E,T,Rez).
-
--- Semester 3 /Functional and Logical Programming/Prolog/evenprod.pl
+++
@@ -1,8 +0,0 @@
-evenProd([],1).
-evenProd([H|T],R):-
-    H mod 2 =:= 0,
-    evenProd(T,RT),
-    R is H*RT.
-evenProd([H|T],R):-
-    H mod 2 =\=0,
-    evenProd(T,R).

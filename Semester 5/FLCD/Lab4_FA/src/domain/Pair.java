bcdb70eceffe8ecfacd0778abd55aceb5257922a
Georgiana Petricele <115110913+georgianapetricele@users.noreply.github.com>
2024-12-29 14:33:17 +0200

    Add files via upload

---
+++ Semester 5/FLCD/Lab4_FA/src/domain/Pair.java
@@ -0,0 +1,54 @@
+package domain;
+
+public class Pair<T1, T2>
+{
+    public T1 first;
+    public T2 second;
+
+
+    public Pair(T1 first, T2 second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T1 getFirst(){
+        return this.first;
+    }
+
+    public T2 getSecond(){
+        return this.second;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+
+        if (!first.equals(pair.first)) {
+            return false;
+        }
+        return second.equals(pair.second);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return 31 * first.hashCode() + second.hashCode();
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return first.toString() + " " + second.toString();
+    }
+}
